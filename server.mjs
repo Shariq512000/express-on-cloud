98e91d151ea48690faf0c39a7971c30b0a428da8
Shariq Siddiqui <99816206+Shariq512000@users.noreply.github.com>
2025-04-23 19:06:59 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -9,6 +9,10 @@ app.get('/' , (req , res) => {
     res.send("Hello World")
 })
 
+app.get('/about', (req, res) => {
+  res.send('About route 🎉 ')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
