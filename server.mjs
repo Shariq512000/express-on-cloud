e7403a2192d0268acb44bbeb6cd8970020a816bf
Meer Quais <meerquais017@gmail.com>
2025-04-23 09:48:44 +0500

    testing server

---
+++ server.mjs
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express()
+const port = 5000;
+
+/// 192.168.200.121:5000
+
+app.get('/' , (req , res) => {
+    console.log(req.ip);
+    res.send("Hello World")
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
